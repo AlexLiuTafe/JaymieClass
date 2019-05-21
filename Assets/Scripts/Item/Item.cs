f2667470560dd05bf3aa68759ed41028e18b4096
AlexLiuTafe <alexliutafe@gmail.com>
2019-05-21 15:56:50 +1000

    Setting Items
    Item script and renaming folder and images name

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,118 @@
+﻿//in this script  you will only need using UnityEngine as we just need the script to connect to unity
+using UnityEngine;
+//this public class doent inherit from MonoBehaviour
+//this script is also referenced by other scripts but never attached to anything
+public class Item
+{
+    //basic variables for items that we need are 
+    #region Private Variables
+    //Identification Number
+    private int _itemID;
+    //Object Name
+    private string _name;
+    //Value of the Object
+    public int _value;
+    //Description of what the Object is
+    private string _description;
+    //Icon that displays when that Object is in an Inventory
+    private Texture2D _icon;
+    //Mesh of that object when it is equipt or in the world
+    private GameObject _mesh;
+    //Enum ItemType which is the Type of object so we can classify them
+    private ItemType _type;
+    private int _heal;
+    private int _damage;
+    private int _armour;
+    private int _amount;
+    #endregion
+
+    #region Public Variables
+    //here we are creating the public versions or our private variables that we can reference and connect to when interacting with items
+    public int ItemID //public Identification Number 
+    {
+        //get the private Identification Number
+        get { return _itemID; }
+        //and set it to the value of our public Identification Number
+        set { _itemID = value; }
+    }
+    public string Name//public Name
+    {
+        //get the private Name
+        get { return _name; }
+        //and set it to the value of our public Name
+        set { _name = value; }
+    }
+
+    public int Value//public Value 
+    {
+        //get the private Value
+        get { return _value; }
+        //and set it to the value of our public Value
+        set { _value = value; }
+    }
+    public string Description//public Description 
+    {
+        //get the private Description
+        get { return _description; }
+        //and set it to the value of our public Description
+        set { _description = value; }
+    }
+    public Texture2D Icon//public Icon 
+    {
+        //get the private Icon
+        get { return _icon; }
+        //and set it to the value of our public Icon
+        set { _icon = value; }
+    }
+    public GameObject Mesh//public Mesh 
+    {
+        //get the private Mesh
+        get { return _mesh; }
+        //and set it to the value of our public Mesh
+        set { _mesh = value; }
+    }
+    public ItemType Type //public Type 
+    {
+        //get the private Type
+        get { return _type; }
+        //and set it to the value of our public Type
+        set { _type = value; }
+    }
+    public int Damage 
+        {
+        get { return _damage; }
+        set { _damage = value; }
+        }
+    public int Armour
+    {
+        get { return _armour; }
+        set { _armour = value; }
+    }
+    public int Heal
+    {
+        get { return _heal; }
+        set { _heal = value; }
+    }
+    public int Amount
+    {
+        get { return _amount; }
+        set { _amount = value; }
+    }
+
+    #endregion
+}
+#region Enums
+//The Global Enum ItemType that we have created categories in
+public enum ItemType
+{
+    Food,
+    Weapon,
+    Apparel,
+    Crafting,
+    Quest,
+    Money,
+    Ingredient,
+    Potion,
+    Scroll
+}
+#endregion
---
+++ Assets/Scripts/Item/ItemData.cs
@@ -0,0 +1,304 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData
+{
+    public static Item CreateItem(int itemID)
+    {
+        string name = "";
+        int value = 0;
+        string description = "";
+        string icon = "";
+        string mesh = "";
+        ItemType type = ItemType.Food;
+        int heal = 0;
+        int damage = 0;
+        int armour = 0;
+        int amount = 0;
+
+        switch (itemID)
+        {
+            #region Food 0-99
+            case 0:
+                name = "Apple";
+                value = 5;
+                description = "Yum Yum";
+                icon = "Food/Apple_Icon";
+                mesh = "Food/Apple_Mesh";
+                type = ItemType.Food;
+                heal = 5;
+                amount = 1;
+                break;
+            case 1:
+                name = "Coconut";
+                value = 30;
+                description = "Yup I am a nut";
+                icon = "Food/Coconut_Icon";
+                mesh = "Food/Coconut_Mesh";
+                type = ItemType.Food;
+                heal = 20;
+                amount = 1;
+                break;
+            case 3:
+                name = "Coffee";
+                value = 15;
+                description = "Zoom Zoom";
+                icon = "Food/Coffee_Icon";
+                mesh = "Food/Coffee_Mesh";
+                type = ItemType.Food;
+                heal = 10;
+                amount = 1;
+                break;
+
+
+            #endregion
+            #region Weapons 100- 199
+            case 100:
+                name = "Kitchen Knife";
+                value = 30;
+                description = "Kitchen USE ONLY";
+                icon = "Weapon/KitchenKnife_Icon";
+                mesh = "Weapon/KitchenKnife_Mesh";
+                type = ItemType.Weapon;
+                damage = 10;
+                amount = 1;
+                break;
+            case 101:
+                name = "Hammer";
+                value = 150;
+                description = "Kitchen Tenderiser";
+                icon = "Weapon/Hammer_Icon";
+                mesh = "Weapon/Hammer_Mesh";
+                type = ItemType.Weapon;
+                damage = 20;
+                amount = 1;
+                break;
+            case 102:
+                name = "Gun";
+                value = 300;
+                description = "Easy Win";
+                icon = "Weapon/Gun_Icon";
+                mesh = "Weapon/Gun_Mesh";
+                type = ItemType.Weapon;
+                damage = 100;
+                amount = 1;
+                break;
+            #endregion
+            #region Apparels 200-299
+            case 200:
+                name = "Shirt";
+                value = 5;
+                description = "Laundry Time";
+                icon = "Apparel/Shirt_Icon";
+                mesh = "Apparel/Shirt_Mesh";
+                type = ItemType.Apparel;
+                armour = 5;
+                amount = 1;
+                break;
+            case 201:
+                name = "Ring";
+                value = 500;
+                description = "My Precious";
+                icon = "Apparel/Ring_Icon";
+                mesh = "Apparel/Ring_Mesh";
+                type = ItemType.Apparel;
+                armour = 1;
+                amount = 1;
+                break;
+            case 202:
+                name = "Boots";
+                value = 5;
+                description = "Not Prada";
+                icon = "Apparel/Boots_Icon";
+                mesh = "Apparel/Boots_Mesh";
+                type = ItemType.Apparel;
+                armour = 5;
+                amount = 1;
+                break;
+            #endregion
+            #region Crafting 300-399
+            case 300:
+                name = "Spring";
+                value = 5;
+                description = "Just Junk";
+                icon = "Crafting/Spring_Icon";
+                mesh = "Crafting/Spring_Mesh";
+                type = ItemType.Crafting;
+                amount = 1;
+                break;
+            case 301:
+                name = "Bomb";
+                value = 10;
+                description = "Let the world Explode";
+                icon = "Crafting/Bomb_Icon";
+                mesh = "Crafting/Bomb_Mesh";
+                type = ItemType.Crafting;
+                amount = 1;
+                break;
+            case 302:
+                name = "Gear";
+                value = 5;
+                description = "Another Junk";
+                icon = "Crafting/Gear_Icon";
+                mesh = "Crafting/Gear_Mesh";
+                type = ItemType.Crafting;
+                amount = 1;
+                break;
+            #endregion
+            #region Quests 400 - 499
+
+            case 400:
+                name = "Eye Ball";
+                value = 10;
+                description = "Missing Pirate's Eye";
+                icon = "Quest/EyeBall_Icon";
+                mesh = "Quest/EyeBall_Mesh";
+                type = ItemType.Quest;
+                amount = 1;
+                break;
+            case 401:
+                name = "Candle";
+                value = 10;
+                description = "Use for praying";
+                icon = "Quest/Candle_Icon";
+                mesh = "Quest/Candle_Mesh";
+                type = ItemType.Quest;
+                amount = 1;
+                break;
+            case 402:
+                name = "Key";
+                value = 10;
+                description = "Unlocking someone house";
+                icon = "Quest/Key_Icon";
+                mesh = "Quest/Key_Mesh";
+                type = ItemType.Quest;
+                amount = 1;
+
+                break;
+            #endregion
+            #region Ingredients 500 - 599
+            case 500:
+                name = "Herb";
+                value = 1;
+                description = "Poison Included";
+                icon = "Ingredient/Herb_Icon";
+                mesh = "Ingredient/Herb_Mesh";
+                type = ItemType.Ingredient;
+                amount = 1;
+                break;
+            case 501:
+                name = "Flower";
+                value = 5;
+                description = "Legend says it gives you luck";
+                icon = "Ingredient/Flower_Icon";
+                mesh = "Ingredient/Flower_Mesh";
+                type = ItemType.Ingredient;
+                amount = 1;
+                break;
+            case 502:
+                name = "Acorn";
+                value = 5;
+                description = "Can be used as bullet";
+                icon = "Ingredient/Acorn_Icon";
+                mesh = "Ingredient/Acorn_Mesh";
+                type = ItemType.Ingredient;
+                amount = 1;
+                break;
+            #endregion
+            #region Potions 600-699
+            case 600:
+                name = "Red Potion";
+                value = 10;
+                description = "Taste like water";
+                icon = "Potion/RedPotion_Icon";
+                mesh = "Potion/RedPotion_Mesh";
+                type = ItemType.Potion;
+                heal = 40;
+                amount = 1;
+                break;
+            case 601:
+                name = "Blue Potion";
+                value = 7;
+                description = "Taste like ocean";
+                icon = "Potion/BluePotion_Icon";
+                mesh = "Potion/BluePotion_Mesh";
+                type = ItemType.Potion;
+                heal = 30;
+                amount = 1;
+                break;
+            case 602:
+                name = "Love Potion";
+                value = 15;
+                description = "Bug Repellent";
+                icon = "Potion/LovePotion_Icon";
+                mesh = "Potion/LovePotion_Mesh";
+                type = ItemType.Potion;
+                heal = 50;
+                amount = 1;
+                break;
+
+            #endregion
+            #region Scrolls 700-899
+            case 700:
+                name = "Fire Scroll";
+                value = 10;
+                description = "Create Fire For Cooking";
+                icon = "Scroll/FireScroll_Icon";
+                mesh = "Scroll/FireScroll_Mesh";
+                type = ItemType.Scroll;
+                damage = 10;
+                amount = 1;
+                break;
+            case 701:
+                name = "Water Scroll";
+                value = 10;
+                description = "Summon rain for gardening";
+                icon = "Scroll/WaterScroll_Icon";
+                mesh = "Scroll/WaterScroll_Mesh";
+                type = ItemType.Scroll;
+                damage = 10;
+                amount = 1;
+                break;
+            case 702:
+                name = "Nature Scroll";
+                value = 10;
+                description = "Instant vegetable for gardening";
+                icon = "Scroll/NatureScroll_Icon";
+                mesh = "Scroll/NatureScroll_Mesh";
+                type = ItemType.Scroll;
+                damage = 10;
+                amount = 1;
+                break;
+            #endregion
+
+            default:
+                itemID = 0;
+                name = "Apple";
+                value = 5;
+                description = "Yum Yum";
+                icon = "Food/Apple_Icon";
+                mesh = "Food/Apple_Mesh";
+                type = ItemType.Food;
+                heal = 5;
+                amount = 1;
+                break;
+        }
+        Item temp = new Item
+        {
+            Name = name,
+            Description = description,
+            ItemID = itemID,
+            Value = value,
+            Damage = damage,
+            Armour = armour,
+            Amount = amount,
+            Heal = heal,
+            Type = type,
+            Mesh = Resources.Load("Prefabs/" + mesh) as GameObject,
+            Icon = Resources.Load("Icons/" + icon) as Texture2D
+        };
+        return temp;
+    }
+
+}
